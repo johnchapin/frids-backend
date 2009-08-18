c9d9d74976897fc46eb0e8703155f756086b0b2c
john <john@70bf4735-b96f-0410-91f7-8b35716c8893>
2009-08-18 21:38:49 +0000

    Tweaked geocoding, displaying only valid or approximate geocoded events on map
    git-svn-id: svn+ssh://boostrot/home/boostrot/svn/fridsbackend/trunk@31 70bf4735-b96f-0410-91f7-8b35716c8893

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,20 +1,39 @@
 class EventsController < ApplicationController
 
   def index
-    @events = Event.paginate( 
+    @events = Event.paginate(
       :page => params[:page],
       :include => [ :lines, :location ],
+      :conditions => [ "locations.status IN ('VALID','APPROXIMATE')" ],
       :order => "event_datetime DESC",
       :per_page => 10
     )
 
     @map = GMap.new("map_div")
     @map.control_init(:large_map => true, :map_type => true)
+    @map.icon_global_init(GIcon.new(:copy_base => GIcon::DEFAULT,
+                                    :icon_size => GSize.new(32,32),
+                                    :image => "/images/gicons/blue-dot.png",
+                                    :shadow => "/images/gicons/msmarker.shadow.png",
+                                    :shadow_size => GSize.new(59,32)),
+                          "blue_icon")
+    @map.icon_global_init(GIcon.new(:copy_base => GIcon::DEFAULT,
+                                    :icon_size => GSize.new(32,32),
+                                    :image => "/images/gicons/green-dot.png",
+                                    :shadow => "/images/gicons/msmarker.shadow.png",
+                                    :shadow_size => GSize.new(59,32)),
+                          "green_icon")
+
+    @blue_icon = Variable.new("blue_icon")
+    @green_icon = Variable.new("green_icon")
+                            
     @map.center_zoom_init([38.0378, -78.4893], 10)
     @events.each do |event|
+      marker_content = render_to_string :partial => "event_marker", :object => event
       @map.overlay_init(
         GMarker.new([event.location.latitude,event.location.longitude],
-          :info_window => "<table><tr><th><b>Address:</b></th><td>#{event.location.clean_address}</td></tr><tr><th><b>Incident:</b></th><td>#{event.lines.collect{|l|l.call_type}.uniq.join("<br/>")}</td></tr><tr><th><b>Units:</b></th><td>#{event.lines.collect{|l|l.unit + (l.arrived_on_scene.nil? ? ", no arrival time available" : ", arrived " + l.arrived_on_scene.strftime("%H:%M:%S %D"))}.join("<br/>")}</td></tr></table>"
+          :info_window => marker_content, :title => event.lines[0].call_type,
+          :icon => (event.location.status == "VALID" ? @green_icon : @blue_icon)
         ))
     end
     respond_to do |format|
