3284b4ca32128a72e2e1faba32eb9666482da290
john <john@70bf4735-b96f-0410-91f7-8b35716c8893>
2009-09-07 01:05:43 +0000

    Added load location migration
    git-svn-id: svn+ssh://boostrot/home/boostrot/svn/fridsbackend/trunk@80 70bf4735-b96f-0410-91f7-8b35716c8893

---
+++ db/migrate/20090904210911_load_location_data.rb
@@ -0,0 +1,13 @@
+require 'active_record/fixtures'
+
+class LoadLocationData < ActiveRecord::Migration
+  def self.up
+    down
+    directory = File.join(File.dirname(__FILE__), 'data') 
+    Fixtures.create_fixtures(directory, "locations")
+  end
+
+  def self.down
+    execute "DELETE FROM locations"
+  end
+end
